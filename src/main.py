45d00ed5b9aaee44cfe987d566f2cfc7c129b444
dxcode <dxcode01@gmail.com>
2025-03-13 18:54:26 -0500

    comments improved

--- src/main.py
+++ src/main.py
@@ -10,26 +10,26 @@ OUTPUT = PATH.joinpath("output.txt")
 
 def main():
     data = []
+    
     # open a file in read mode 
     with open(f"{INPUT.absolute()}" ,"r") as file:
 
             for line in file :
-                # IF new line char at the end convert string to array and replace it 
-                new_line = re.findall(f"\n" , line)
+    
+                new_line_chars = re.findall(f"\n" , line)
                 
-                #  last line in the stream
-                if(new_line.__len__() == 0):
+                #  IF line does not have a new line char , it is the the stream's end 
+                if(new_line_chars.__len__() == 0):
                     data.append(line)
                     break    
                 
-                # regular line in the stream
-                elif(new_line.__len__() > 0):
-                    holder = line.split()
-                    holder = "".join(holder) + ","     
-                    data.append(holder)    
+                # ELSE it is a regular line in the stream , need to replace new line char for comma 
+                holder = line.split()
+                holder = "".join(holder) + ","     
+                data.append(holder)    
 
 
-
+    # create output file and append the modified input's content 
     with open(OUTPUT ,"a") as file :
          for line in data :
               file.write(line)
